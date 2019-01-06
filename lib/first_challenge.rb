bf1477ee551cce3daa64d4bcb326939566b0bb72
Stephen Wright <scw248@gmail.com>
2019-01-06 01:32:45 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,21 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      
+      data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
+          
+          value.delete_if do |flavor|
+            flavor == "strawberry"
+              end
+            end
+          end
+      end
+  end
 
-
+contacts
   #remember to return your newly altered contacts hash!
   contacts
 end
